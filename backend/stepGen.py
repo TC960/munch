89628ea04aa67c73f5d0c675dd1605146339a141
Aryen Singhal <aryensinghal@gmail.com>
2024-10-18 22:15:26 -0700

    update recipe prompts

--- backend/stepGen.py
+++ backend/stepGen.py
@@ -11,15 +11,16 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 parser = StrOutputParser()
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", api_key=GEMINI_API_KEY)
 
-recipe = "Broccoli Cheddar Quiche"
-ingredients = "ggs, onions, tomatoes, bell peppers, cheese, milk, corn, brocolli, bread"
+recipe = "Cheese Quesadilla with Corn and Peppers"
+ingredients = "eggs, onions, tomatoes, bell peppers, cheese, milk, corn, brocolli, bread"
 
 template = """You are a generative AI model with ubiquitous knowledge all possible 
  recipes using all sorts of combinations of foods. You are also very clear, coherent
- and concise with the way you respond, not giving too much bullshit. Specify for 
- how many people the food will suffice. Generate me all the steps required in 
- completing the recipe. The recipe that I need instructions for is {recipe},
- using {ingredients}  ]
+ and concise with the way you respond, not giving too much bullshit.
+ Generate me all the steps required in completing the recipe, for a serving size of 1 person.
+ The recipe that I need instructions for is {recipe},
+ using only the ingredients: {ingredients}.
+ Keep in mind that the recipe is for a college student, it should be easy to make.
 """
 
 chatPrompt = ChatPromptTemplate.from_template(template)
