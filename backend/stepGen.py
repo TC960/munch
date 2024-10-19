c3276f18eeb9c055f59333d75173c04420d43267
Aryen Singhal <aryensinghal@gmail.com>
2024-10-18 21:57:21 -0700

    third times the charm

--- backend/stepGen.py
+++ backend/stepGen.py
@@ -11,19 +11,20 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 parser = StrOutputParser()
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", api_key=GEMINI_API_KEY)
 
-ingredients = "eggs, onions, tomatoes, bell peppers, cheese, milk, corn, brocolli, bread"
+recipe = "Broccoli Cheddar Quiche"
+ingredients = "ggs, onions, tomatoes, bell peppers, cheese, milk, corn, brocolli, bread"
 
 template = """You are a generative AI model with ubiquitous knowledge all possible 
  recipes using all sorts of combinations of foods. You are also very clear, coherent
- and concise with the way you respond, not giving too much bullshit. Generate me a list of all possible 
- recipes using the ingredients provided. Keep the format grouping recipes on the time of day. 
- Omit all instructions, only give names. Add the preparation time to each recipe.
- The ingredients are {ingredients}
+ and concise with the way you respond, not giving too much bullshit. Specify for 
+ how many people the food will suffice. Generate me all the steps required in 
+ completing the recipe. The recipe that I need instructions for is {recipe},
+ using {ingredients}  ]
 """
 
 chatPrompt = ChatPromptTemplate.from_template(template)
 chain = chatPrompt | llm | parser
 
-result = chain.invoke({"ingredients":ingredients})
-with open("Output.txt","w") as file:
+result = chain.invoke({"recipe":recipe, "ingredients":ingredients})
+with open("Output2.txt","w") as file:
     file.write(result)
