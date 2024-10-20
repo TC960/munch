e2a129d770f082e96986ec348b11d3c8f40b5273
Aryen Singhal <aryensinghal@gmail.com>
2024-10-20 09:05:01 -0700

    get recipe list

--- frontend/munch/lib/main.dart
+++ frontend/munch/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert'; // required to encode/decode json data
+import 'package:http/http.dart' as http;
+import 'string_extensions.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,27 +15,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Munch App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0x00632CB5)),
+        typography: Typography.material2021(),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      home: const MyHomePage(title: 'Munch'),
     );
   }
 }
@@ -55,71 +48,147 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<dynamic> ingredients = [];
+  List<dynamic> breakfast = [];
+  List<dynamic> lunch = [];
+  List<dynamic> dinner = [];
+  List<dynamic> snacks = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void fetchRecipeList(Function callback) async {
+    print("Fetch Started");
+    const url = 'http://10.0.2.2:8000/get_recipe_list';
+    var request = http.MultipartRequest('POST', Uri.parse(url));
+    request.files.add(http.MultipartFile.fromBytes('image',
+        (await rootBundle.load('images/groceries2.jpg')).buffer.asUint8List(),
+        filename: 'image'));
+    var streamedResponse = await request.send();
+    var response = await http.Response.fromStream(streamedResponse);
+    var json = jsonDecode(response.body);
+    print(response.body);
+    print(json);
+    if (response.statusCode == 200) {
+      print("Upload successful");
+      setState(() {
+        ingredients = json['detected_ingredients'];
+        breakfast = json['breakfast'];
+        lunch = json['lunch'];
+        dinner = json['dinner'];
+        snacks = json['snacks'];
+      });
+      callback();
+      print("Fetch Completed");
+    } else {
+      print("Error getting recipe list");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  ingredients = [];
+                  breakfast = [];
+                  lunch = [];
+                  dinner = [];
+                  snacks = [];
+                });
+                fetchRecipeList(() {
+                  setState(() {});
+                  Navigator.pop(context);
+                  showModalBottomSheet(
+                    context: context,
+                    builder: buildBottomSheet,
+                    isScrollControlled: true,
+                    showDragHandle: true,
+                    enableDrag: true,
+                    useSafeArea: true,
+                  );
+                });
+                showModalBottomSheet(
+                  context: context,
+                  builder: buildBottomSheet,
+                  isScrollControlled: true,
+                  showDragHandle: true,
+                  enableDrag: true,
+                  useSafeArea: true,
+                );
+              },
+              child: Image.asset(
+                'images/shutter_button.png',
+                width: 88,
+                height: 88,
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget buildBottomSheet(BuildContext context) {
+    if (ingredients.isEmpty) {
+      return const SizedBox(
+        width: double.infinity,
+        height: 200,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
+      var categoryStrings = ["Breakfast", "Lunch", "Dinner", "Snacks"];
+      var categoryObjects = [breakfast, lunch, dinner, snacks];
+      return ListView.builder(
+        itemCount: 4,
+        itemBuilder: (context, index) {
+          var category = categoryObjects[index];
+          return Column(
+            children: [
+              Text(
+                categoryStrings[index].toTitleCase(),
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge
+                    ?.apply(color: Theme.of(context).colorScheme.primary),
+              ),
+              ListView.builder(
+                primary: false,
+                shrinkWrap: true,
+                itemCount: category.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    onTap: () {
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //         builder: (context) => InstructionsPage(
+                      //               title: 'Munch',
+                      //               recipe:
+                      //                   category[index]['recipe'].toString(),
+                      //               ingredients: ingredients,
+                      //             )));
+                    },
+                    title: Text(
+                        category[index]['recipe'].toString().toTitleCase()),
+                    subtitle:
+                        Text("Prep time: ${category[index]['prep_time']} mins"),
+                  );
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 }
